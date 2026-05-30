800cdc9c0d914a5f8b55a71df1a14884ee1cdb79
MSAA24 <marcos.acuna@estudiantes.unahur.edu.ar>
2026-05-30 12:28:14 -0300

    feat(moderacion): umbral M=1, suspensión por umbral, estado revocada/revocado, endpoint verificar denuncia

--- db/migrations/20260528000003-create-configuraciones-moderacion.js
+++ db/migrations/20260528000003-create-configuraciones-moderacion.js
@@ -43,7 +43,7 @@ module.exports = {
       },
       {
         clave: 'M_DENUNCIAS_VERIFICADAS',
-        valor: 3,
+        valor: 1,
         descripcion:
           'Cantidad de denuncias verificadas para suspender automáticamente un material',
       },
