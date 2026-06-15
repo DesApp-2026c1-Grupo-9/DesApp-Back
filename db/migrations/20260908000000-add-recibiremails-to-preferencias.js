2c359bc3ed29b59b353a82fee1e6487d9a5c5a2d
unknown <fj.villalba@outlook.com>
2026-06-15 15:57:18 -0300

    agregar notificaciones por mail

---
+++ db/migrations/20260908000000-add-recibiremails-to-preferencias.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(
+      'PreferenciasEstudiante',
+      'recibirEmails',
+      {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+        allowNull: false,
+      }
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn(
+      'PreferenciasEstudiante',
+      'recibirEmails'
+    );
+  },
+};
